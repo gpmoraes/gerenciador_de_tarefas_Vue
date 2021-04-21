e4a55422554c9b09413b530c95e513d996d8e6ed
Guilherme Moraes <moraes.guilhermep@gmail.com>
2021-04-21 16:24:01 +0000

    Traalhando com instancias do Axios na aplicação

--- task-manager/src/axios.js
+++ task-manager/src/axios.js
@@ -1,6 +1,25 @@
 import axios from 'axios'
 import config from './config/config'
 
-axios.defaults.baseURL = config.apiURL
+//================ Forma 01 ==========================
+// Uma das for mas que pode ser usada
 
-export default axios
+// axios.defaults.baseURL = config.apiURL
+// export default axios
+
+//================ Forma 02 ==========================
+// Pode ser configurado dessa forma, aqui não é necessário utilizar o export default
+// Recebe um objeto com as configurações que se deseja setar
+
+// export default axios.create({
+//     baseURL: config.apiURL
+// })
+
+//================ Forma 03 ==========================
+// Criase uma nova instancia do axios para que possa ser utilizada
+// Com isso a configuração pode ser feita em outras partes da aplicação 
+
+const instace = axios.create({})
+instace.defaults.baseURL = config.apiURL
+
+export default instace
